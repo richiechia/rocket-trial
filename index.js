1c789eed2549008d93a9fd930d1380d5dc671206
richiechia <richiechia@gmail.com>
2022-01-29 11:24:22 +0800

    Added data.json & editted index.js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 let name = process.argv[2]
 console.log(`hello ${name.toUpperCase()}! How are you?`);
+
+
+import { writeFile } from 'fs';
+let name = process.argv[2];
+writeFile('data.json', name, (err) => console.error('err ->', err));
